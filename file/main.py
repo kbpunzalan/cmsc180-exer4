887f6cbe6f9a58b7340d6b8a76af22e6edfb2ccf
kbpunzalan <kbpunzalan@up.edu.ph>
2023-05-27 21:55:05 +0800

    file changes

--- file/main.py
+++ file/main.py
@@ -4,16 +4,17 @@ s = int(input("Enter s: "))
 import master
 import slave
 
-with open("config.txt") as f:
-    n = int(f.readline())
-    t_minus_1 = int(f.readline())
-    port = int(f.readline())
-    host = f.readline()
 
+if __name__ == "__main__":
+    with open("config.txt") as f:
+        n = int(f.readline())
+        t_minus_1 = int(f.readline())
+        port = int(f.readline())
+        host = f.readline()
 
-if s == 0:
-    print("Sever")
-    master.main(n, t_minus_1, host, port)
-else:
-    print("Client")
-    slave.main(n, host, port)
+    if s == 0:
+        print("Sever")
+        master.main(n, t_minus_1, host, port)
+    else:
+        print("Client")
+        slave.main(n, host, port)
